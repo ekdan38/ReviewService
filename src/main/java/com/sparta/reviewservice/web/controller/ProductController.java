58362b008ca03c0003385e16df05729ab6959f9d
JeongHwa <ekdan31@gmail.com>
2024-12-12 23:00:13 +0900

    FEAT --> Product 생성 응답 수정

--- src/main/java/com/sparta/reviewservice/web/controller/ProductController.java
+++ src/main/java/com/sparta/reviewservice/web/controller/ProductController.java
@@ -1,10 +1,9 @@
 package com.sparta.reviewservice.web.controller;
 
-import com.sparta.reviewservice.domain.dto.ProductResponseDto;
-import com.sparta.reviewservice.domain.entity.Product;
 import com.sparta.reviewservice.domain.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +15,8 @@ public class ProductController {
 
     // 상품 등록
     @PostMapping("/products")
-    public ProductResponseDto createProducts() {
-        return productService.createProducts();
+    public ResponseEntity<?> createProducts() {
+        return ResponseEntity.ok().body(productService.createProducts());
     }
-
 
 }
