c8501a08ffde95e5c0993dffca73683f6ce5c13b
JeongHwa <ekdan31@gmail.com>
2024-12-13 13:01:47 +0900

    FIX --> 리뷰 등록 동시성 해결

--- src/main/java/com/sparta/reviewservice/domain/repository/ProductRepository.java
+++ src/main/java/com/sparta/reviewservice/domain/repository/ProductRepository.java
@@ -2,8 +2,17 @@ package com.sparta.reviewservice.domain.repository;
 
 
 import com.sparta.reviewservice.domain.entity.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id = :productId")
+    Optional<Product> findByIdWithLock(@Param("productId") Long productId);
 }
